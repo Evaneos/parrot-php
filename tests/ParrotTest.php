7d5bafce78cec9d8d3517a92b5158e735d539ec3
remi-san <remi.san@gmail.com>
2017-05-11 15:46:32 +0200

    tests: add tests to check uncovered cases

--- tests/ParrotTest.php
+++ tests/ParrotTest.php
@@ -4,11 +4,38 @@ namespace Parrot;
 
 class ParrotTest extends \PHPUnit_Framework_TestCase
 {
-
     /** @test */
     public function get_speed_of_european_parrot()
     {
         $parrot = new Parrot(ParrotTypeEnum::EUROPEAN, 0, 0, false);
+        $this->assertEquals(12.0, $parrot->getSpeed());
+    }
+
+    /** @test */
+    public function get_speed_of_european_parrot_with_one_coconut()
+    {
+        $parrot = new Parrot(ParrotTypeEnum::EUROPEAN, 1, 0, false);
+        $this->assertEquals(12.0, $parrot->getSpeed());
+    }
+
+    /** @test */
+    public function get_speed_of_european_parrot_with_voltage()
+    {
+        $parrot = new Parrot(ParrotTypeEnum::EUROPEAN, 0, 1, false);
+        $this->assertEquals(12.0, $parrot->getSpeed());
+    }
+
+    /** @test */
+    public function get_speed_of_nailed_european_parrot()
+    {
+        $parrot = new Parrot(ParrotTypeEnum::EUROPEAN, 0, 0, true);
+        $this->assertEquals(12.0, $parrot->getSpeed());
+    }
+
+    /** @test */
+    public function get_speed_of_african_parrot_with_no_coconuts()
+    {
+        $parrot = new Parrot(ParrotTypeEnum::AFRICAN, 0, 0, false);
         $this->assertEquals(12.0, $parrot->getSpeed());
     }
 
@@ -19,11 +46,17 @@ class ParrotTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(3.0, $parrot->getSpeed());
     }
 
+    /** @test */
+    public function get_speed_of_african_parrot_with_voltage()
+    {
+        $parrot = new Parrot(ParrotTypeEnum::AFRICAN, 0, 1, false);
+        $this->assertEquals(12.0, $parrot->getSpeed());
+    }
 
     /** @test */
-    public function get_speed_of_african_parrot_with_no_coconuts()
+    public function get_speed_of_nailed_african_parrot()
     {
-        $parrot = new Parrot(ParrotTypeEnum::AFRICAN, 0, 0, false);
+        $parrot = new Parrot(ParrotTypeEnum::AFRICAN, 0, 0, true);
         $this->assertEquals(12.0, $parrot->getSpeed());
     }
 
@@ -35,9 +68,30 @@ class ParrotTest extends \PHPUnit_Framework_TestCase
     }
 
     /** @test */
+    public function get_speed_norwegian_blue_parrot_not_nailed_with_no_voltage()
+    {
+        $parrot = new Parrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 0, false);
+        $this->assertEquals(0.0, $parrot->getSpeed());
+    }
+
+    /** @test */
+    public function get_speed_norwegian_blue_parrot_not_nailed_with_one_volt()
+    {
+        $parrot = new Parrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 1, false);
+        $this->assertEquals(12.0, $parrot->getSpeed());
+    }
+
+    /** @test */
     public function get_speed_norwegian_blue_parrot_not_nailed_high_voltage()
     {
         $parrot = new Parrot(ParrotTypeEnum::NORWEGIAN_BLUE, 0, 4, false);
         $this->assertEquals(24.0, $parrot->getSpeed());
+    }
+
+    /** @test */
+    public function get_speed_norwegian_blue_parrot_not_nailed_with_one_volt_and_one_coconut()
+    {
+        $parrot = new Parrot(ParrotTypeEnum::NORWEGIAN_BLUE, 1, 1, false);
+        $this->assertEquals(12.0, $parrot->getSpeed());
     }
 }
