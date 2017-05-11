24c7e8d94d9336a0b8bb78a9e2ee5753a0ccaeeb
Quentin <quentin.pautrat@gmail.com>
2017-05-11 16:13:16 +0200

    Add test to norwegian parrot to improve coverage.

--- tests/NorwegianBlueParrotTest.php
+++ tests/NorwegianBlueParrotTest.php
@@ -4,15 +4,21 @@ namespace Parrot;
 
 class NorwegianBlueParrotTest extends \PHPUnit_Framework_TestCase{
 
+    public function it_should_not_fly_faster_above_maximum_speed()
+    {
+        $parrot = new NorwegianBlueParrot(3.0, false);
+        $this->assertEquals(24.0, $parrot->getSpeed());
+    }
+
     /** @test */
     public function it_should_not_fly_if_it_is_nailed() {
-        $parrot = new NorwegianBlueParrot(0,true);
+        $parrot = new NorwegianBlueParrot(0, true);
         $this->assertEquals(0.0, $parrot->getSpeed());
     }
 
     /** @test */
     public function it_should_fly_faster_with_voltage() {
-        $parrot = new NorwegianBlueParrot(4, false);
-        $this->assertEquals(24.0, $parrot->getSpeed());
+        $parrot = new NorwegianBlueParrot(1.5, false);
+        $this->assertEquals(18.0, $parrot->getSpeed());
     }
 }
